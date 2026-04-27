8307d062af890ffc4311a28887a7bd8b30cfcbf9
sakshirao8577 <raosakshi8577@gmail.com>
2026-04-27 10:43:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var express = require("express");
 var app = express();
-var api = require("./student");
-app.use("/students", api);
-app.listen(7001);
+app.use(express.urlencoded());
+app.set("view engine",'ejs');
+app.get("/",(req,res)=>{
+    res.render("form");
+})
+app.post("/result",(req,res)=>{
+    var n=req.body.sn;
+    var m=req.body.marks;
+    res.render("result",{n,m});
+})
+app.listen(8009);
